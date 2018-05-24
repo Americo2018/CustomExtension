cf527972eea09d25e9bde60d4db8400980c45952
Americo2018 <americo.perez@outlook.com>
2018-05-24 19:53:21 +0200

    First Commit

---
+++ src/extensions/customExtensions/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface ICustomExtensionsApplicationCustomizerStrings {
+  Title: string;
+}
+
+declare module 'CustomExtensionsApplicationCustomizerStrings' {
+  const strings: ICustomExtensionsApplicationCustomizerStrings;
+  export = strings;
+}
